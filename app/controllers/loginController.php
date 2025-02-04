96fd42537446ab7984121b47621c6ef1e11d09da
elysdan <elysdan@vivaldi.net>
2025-02-04 13:43:01 -0400

    validaciones login

--- app/controllers/loginController.php
+++ app/controllers/loginController.php
@@ -4,6 +4,9 @@ include('../../debug/errores.php');
 include('../config/config.php');
 include('../config/conn.php');
 
+//echo password_hash('123456', PASSWORD_DEFAULT);
+//die();
+
 try {
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Para obtencion de errores de PDO
@@ -20,6 +23,29 @@ try {
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':correo', $correo, PDO::PARAM_STR); //Evita injeccion SQL
     $stmt->execute();
+    $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //var_dump($usuarios);
+    //die();
+    $contador = 0;
+        foreach($usuarios as $u){
+            $contador += 1;
+            $pass = $u['contrasena'];
+
+           // var_dump($pass);
+            //die();
+        }
+
+        if($contador == "0"){ // Verifica datos existentes
+            echo "Los datos ingresados no pertenecen a un usuario Activo, por favor verifique nuevamente los datos ingresados";
+        }else{
+            if (password_verify($password, $pass)) {
+                echo "Contrasena correcta";
+            }else{
+                //die($password . '  ' . $pass);
+                echo "Contrasena incorrecta";
+            }
+        }     
 
 } catch (PDOException $e) {
     echo "Error de base de datos: " . $e->getMessage();
